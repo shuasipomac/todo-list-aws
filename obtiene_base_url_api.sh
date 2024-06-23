2565dbeee5319883b0b05e11ab5ec79b72d726da
Silvia Huasipoma Casas <167950094+shuasipomac@users.noreply.github.com>
2024-06-23 02:43:38 -0500

    Update obtiene_base_url_api.sh

--- obtiene_base_url_api.sh
+++ obtiene_base_url_api.sh
@@ -31,7 +31,7 @@ if [ "$stage" == "default" ]; then
     outputs=$(aws cloudformation describe-stacks --stack-name todo-list-aws --region $region | jq '.Stacks[0].Outputs')
         
 else    
-    //outputs=$(aws cloudformation describe-stacks --stack-name todo-list-aws-$stage --region $region | jq '.Stacks[0].Outputs')
+    # outputs=$(aws cloudformation describe-stacks --stack-name todo-list-aws-$stage --region $region | jq '.Stacks[0].Outputs')
       outputs=$(aws cloudformation describe-stacks --stack-name $stage-todo-list-aws --region $region | jq '.Stacks[0].Outputs')
 fi
 
@@ -50,24 +50,24 @@ echo $BASE_URL_API > base_url_api.tmp
 
 
 
-// if [ "$#" -lt 2 ]; then
-//  echo "Usage: $0 <stage> <region>"
-//  exit 1
-// fi
+# if [ "$#" -lt 2 ]; then
+#  echo "Usage: $0 <stage> <region>"
+#  exit 1
+# fi
 
-// stage=$1
-// region=$2
+# stage=$1
+# region=$2
 
-// echo "obtiene_base_url_api.sh --> Input 1 'stage' value: $stage"
-// echo "obtiene_base_url_api.sh --> Input 2 'region' value: $region"
+# echo "obtiene_base_url_api.sh --> Input 1 'stage' value: $stage"
+# echo "obtiene_base_url_api.sh --> Input 2 'region' value: $region"
 
 
-// outputs=$(aws cloudformation describe-stacks --stack-name $stage-todo-list-aws --region $region | jq '.Stacks[0].Outputs')
+# outputs=$(aws cloudformation describe-stacks --stack-name $stage-todo-list-aws --region $region | jq '.Stacks[0].Outputs')
 
-// extract_value() {
-//    echo "$outputs" | jq -r ".[] | select(.OutputKey==\"$1\") | .OutputValue"
-// }
+# extract_value() {
+#    echo "$outputs" | jq -r ".[] | select(.OutputKey==\"$1\") | .OutputValue"
+# }
 
-// BASE_URL_API=$(extract_value "BaseUrlApi")
+# BASE_URL_API=$(extract_value "BaseUrlApi")
 
-// echo $BASE_URL_API > base_url_api.tmp
+# echo $BASE_URL_API > base_url_api.tmp
