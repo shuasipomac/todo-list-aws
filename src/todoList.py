49d354dd798812409c9e716f1a5de548e17a0dee
Silvia Huasipoma Casas <167950094+shuasipomac@users.noreply.github.com>
2024-06-29 20:04:22 -0500

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -18,7 +18,7 @@ def get_table(dynamodb=None):
             dynamodb = boto3.resource("dynamodb")
             # fetch todo from the database
             table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    return table
+            return table
 
 
 def get_item(key, dynamodb=None):
